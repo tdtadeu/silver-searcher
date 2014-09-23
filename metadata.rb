95473a3729082285cb904b66e0761e0350c1b88f
Thiago Dias <tdtadeu@gmail.com>
2014-09-23 16:55:11 -0300

    Adding apt dependency on metadata

--- metadata.rb
+++ metadata.rb
@@ -1,8 +1,10 @@
 name             'silver_searcher'
 maintainer       'tdtadeu'
 maintainer_email 'tdtadeu@gmail.com'
-license          'All rights reserved'
+license          'MIT'
 description      'Installs/Configures silver_searcher'
-long_description 'Installs/Configures silver_searcher'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.0.1'
+
+depends          'apt'
 
