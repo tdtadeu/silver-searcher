bfe58a5c848d71e8a24a7b0d3aa55034b9ba4486
Thiago Dias <tdtadeu@gmail.com>
2014-09-23 17:50:10 -0300

    Install from source

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'tdtadeu@gmail.com'
 license          'MIT'
 description      'Installs/Configures silver_searcher'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.0.1'
+version          '0.1.0'
 
 depends          'apt'
 
--- recipes/default.rb
+++ recipes/default.rb
@@ -1,12 +1,39 @@
-apt_repository "silver-searcher" do
-  uri          'http://ppa.launchpad.net/pgolm/the-silver-searcher'
-  distribution node['lsb']['codename']
-  components   ['main']
-  keyserver    'hkp://keyserver.ubuntu.com:80'
-  key          '841AB38C'
-  action       :add
-end
-
 include_recipe "apt"
 
-package "the-silver-searcher"
+def setup_ag
+  install_dependencies
+
+  download_ag_repository
+
+  install_ag
+end
+
+def install_dependencies
+  package "automake"
+  package "pkg-config"
+  package "libpcre3-dev"
+  package "zlib1g-dev"
+  package "liblzma-dev"
+end
+
+def download_ag_repository
+  git "Checkout Code" do
+    repository "git://github.com/ggreer/the_silver_searcher.git"
+    reference "master"
+    action :checkout
+    destination "/usr/local/src/the_silver_searcher"
+  end
+end
+
+def install_ag
+  bash "install silver searcher" do
+    cwd "/usr/local/src/the_silver_searcher"
+    code <<-EOH
+      PKG_CONFIG_PATH=/usr/local/lib64/pkgconfig ./build.sh &&
+      make install
+    EOH
+    creates "/usr/local/bin/ag"
+  end
+end
+
+setup_ag
