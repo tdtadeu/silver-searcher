616f484cbfc261f9ee818560f5e58b86fc679bf1
Thiago Dias <tdtadeu@gmail.com>
2014-09-23 17:17:41 -0300

    Adding silver searcher repo to apt-repo

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,3 +1,12 @@
+apt_repository "silver-searcher" do
+  uri          'http://ppa.launchpad.net/pgolm/the-silver-searcher'
+  distribution node['lsb']['codename']
+  components   ['main']
+  keyserver    'hkp://keyserver.ubuntu.com:80'
+  key          '841AB38C'
+  action       :add
+end
+
 include_recipe "apt"
 
-package "silversearcher-ag"
+package "the-silver-searcher"
