c2534e4789468e890bf78753051da1945c47f60c
Thiago Dias <tdtadeu@gmail.com>
2014-09-23 16:51:50 -0300

    Version 0.0.1

---
+++ README.md
@@ -0,0 +1,3 @@
+# Silver Searcher Ag
+
+This cookbook install silver searcher ag for development purposes
---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             'silver_searcher'
+maintainer       'tdtadeu'
+maintainer_email 'tdtadeu@gmail.com'
+license          'All rights reserved'
+description      'Installs/Configures silver_searcher'
+long_description 'Installs/Configures silver_searcher'
+version          '0.0.1'
+
